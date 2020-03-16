39c6879673b4caca13f65142bc928b14194a7e0d
dylan <fredyfredbob@gmail.com>
2020-03-15 20:34:00 -0700

    First

---
+++ app/src/main/java/com/example/wevotefinal/Candidate_3.java
@@ -0,0 +1,67 @@
+package com.example.wevotefinal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Candidate_3 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidate_3);
+
+        {
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            drawer = findViewById(R.id.drawer_layout);
+            //*****
+            NavigationView navigationView = findViewById(R.id.nav_view);//listener for navigation method
+            navigationView.setNavigationItemSelectedListener(this);
+            //*****
+
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                    R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Intent intent_home = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent_home);
+                break;
+
+            case R.id.nav_logout:
+                Intent intent_logout = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_logout);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
